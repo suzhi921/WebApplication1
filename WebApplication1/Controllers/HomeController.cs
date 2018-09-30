0feb4c23880cbe752bb73c7a9c84bda1505ce553
steven <steven@STEVEN-PC>
2018-09-30 20:44:18 +0800

    ree

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace WebApplication1.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index()
-        {
+        {   
             //1111111111111
             return View();
         }
